15d42ebe7c2e24383a5a0ec26b3f0a7ca2f426e4
parth299 <sainiparth299@gmail.com>
2024-05-30 23:54:58 +0530

    fixed some bugs in controllers and cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,6 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
+        fs.unlinkSync(localFilePath);
         // file has been uploaded successfull
         console.log("file is uploaded on cloudinary ", response.url);
         return response;
